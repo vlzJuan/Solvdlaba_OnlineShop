da05de5feec97cbaa9c055bf85f6ce143c1e9d82
vlzJuan <valencellojuan@gmail.com>
2024-09-13 10:16:41 -0300

    - Fixed methods and added documentation to properly use this LinkedList. - Removed shadowing-parameter E from the node declaration. - Added a 'pop' method, to delete a node with no care for its actual value.

--- src/main/java/solvd/laba/customstructures/LinkedList.java
+++ src/main/java/solvd/laba/customstructures/LinkedList.java
@@ -8,10 +8,12 @@ import solvd.laba.exceptions.IndexOutOfRangeException;
  */
 public class LinkedList<E> {
 
-    // Node used within this linked list
-    private class Node<E> {
+    /**
+     * Private node class, used within this list to wrap stored data.
+     */
+    private class Node {
         E data;
-        Node<E> next;
+        Node next;
 
         Node(E data) {
             this.data = data;
@@ -19,12 +21,10 @@ public class LinkedList<E> {
         }
     }
 
-    private Node<E> head;
-    private int size;
+    private Node head;
 
     public LinkedList() {
         this.head = null;
-        this.size = 0;
     }
 
     /**
@@ -33,73 +33,97 @@ public class LinkedList<E> {
      * @param element   , the data to be added in that position (Wrapped by a node)
      */
     public void add(int index, E element) {
-        if (index < 0 || index > size) {
-            throw new IndexOutOfRangeException("Index: " + index + ", Size: " + size);
+        if (index < 0 || index > this.size()) {
+            throw new IndexOutOfRangeException("Index ("+ index + ") not in range ("
+                    + this.size() + ").");
         }
-        Node<E> newNode = new Node<>(element);
+        Node aux = new Node(element);
         if (index == 0) {
-            newNode.next = head;
-            head = newNode;
+            aux.next = head;
+            head = aux;
         } else {
-            Node<E> current = head;
+            Node current = head;
             for (int i = 0; i < index - 1; i++) {
                 current = current.next;
             }
-            newNode.next = current.next;
-            current.next = newNode;
+            aux.next = current.next;
+            current.next = aux;
         }
-        size++;
     }
 
-    // Remove element by index
+    /**
+     * Method to remove an element by the index.
+     * @param index , the position in the linked list of the data to pop.
+     * @return  the value stored in the removed index.
+     */
     public E remove(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        if (index < 0 || index >= this.size()) {
+            throw new IndexOutOfRangeException("Index ("+ index + ") not in range ("
+                    + this.size() + ").");
         }
-        Node<E> removedNode;
+        Node removedNode;
         if (index == 0) {
             removedNode = head;
             head = head.next;
         } else {
-            Node<E> current = head;
+            Node current = head;
             for (int i = 0; i < index - 1; i++) {
                 current = current.next;
             }
             removedNode = current.next;
             current.next = current.next.next;
         }
-        size--;
         return removedNode.data;
     }
 
-    // Remove element by value
-    public boolean remove(Object o) {
-        if (head == null) return false;
 
-        if (head.data.equals(o)) {
-            head = head.next;
-            size--;
-            return true;
-        }
+    /**
+     * Method to remove an element by the index. When using pop, the return value
+     * of the popped value is irrelevant.
+     * @param index , the position in the linked list of the data to pop.
+     */
+    public void pop(int index) {
+        this.remove(index);
+    }
 
-        Node<E> current = head;
-        while (current.next != null) {
-            if (current.next.data.equals(o)) {
-                current.next = current.next.next;
-                size--;
-                return true;
+
+    /**
+     * Method to remove an object if it is present in the list.
+     * This will eliminate the first ocurrence of said object in the list.
+     * @param removable , the object to remove.
+     * @return  false if the object is not present, true if it was removed correctly.
+     */
+    public boolean remove(Object removable) {
+        boolean ret = false;
+
+        if (head != null) {
+
+            if (head.data.equals(removable)) {
+                head = head.next;
+                ret = true;
             }
-            current = current.next;
+            else {
+                Node current = head;
+                while (current.next != null) {
+                    if (current.next.data.equals(removable)) {
+                        current.next = current.next.next;
+                        ret = true;
+                        break;
+                    }
+                    current = current.next;
+                }
+            }
         }
-        return false;
+        return ret;
     }
 
     // Set element at a specific index
     public void set(int index, E element) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        if (index < 0 || index >= this.size()) {
+            throw new IndexOutOfRangeException("Index ("+ index + ") not in range ("
+                    + this.size() + ").");
         }
-        Node<E> current = head;
+        Node current = head;
         for (int i = 0; i < index; i++) {
             current = current.next;
         }
@@ -108,40 +132,48 @@ public class LinkedList<E> {
 
     // Get element by index
     public E get(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        if (index < 0 || index >= this.size()) {
+            throw new IndexOutOfRangeException("Index ("+ index + ") not in range ("
+                    + this.size() + ").");
         }
-        Node<E> current = head;
+        Node current = head;
         for (int i = 0; i < index; i++) {
             current = current.next;
         }
         return current.data;
     }
 
-    // Clear the linked list
+    // Modify. I'm unsure if letting the garbage collector is the most optimal way to do this.
     public void clear() {
         head = null;
     }
 
     public int size() {
         int ret = 0;
-        Node<E> aux = head;
+        Node aux = head;
         while(aux!=null){
             ret = ret + 1;
             aux = aux.next;
         }
-        return size;
+        return ret;
     }
 
-    // Print all elements in the list (for testing)
-    public void printList() {
-        Node<E> current = head;
-        while (current != null) {
-            System.out.print(current.data + " -> ");
-            current = current.next;
+    public String toString(){
+        StringBuilder ret = new StringBuilder("Lista: {\n");
+        Node current = head;
+        if (head == null){
+            ret.append("{Lista vacia}");
         }
-        System.out.println("null");
+        else{
+            ret.append(current.data.toString());
+            while(current.next!=null){
+                current = current.next;
+                ret.append(" -> ");
+                ret.append(current.data.toString());
+            }
+            ret.append("}");
+        }
+        return ret.toString();
     }
-
 
 }
