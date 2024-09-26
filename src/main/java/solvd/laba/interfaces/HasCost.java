c4a2158d44e67f093a2a04e998a702b62f3eb0d6
vlzJuan <valencellojuan@gmail.com>
2024-09-26 03:07:45 -0300

    Interface declared to standardize the double cost getter for Product, Cart and CartProduct

---
+++ src/main/java/solvd/laba/interfaces/HasCost.java
@@ -0,0 +1,11 @@
+package solvd.laba.interfaces;
+
+
+/**
+ * Interface applied to all classes that can retrieve a cost of themselves.
+ */
+public interface HasCost {
+
+    public double getCost();
+
+}
